82968d0b724d4e5a8771ebb0f1d27dd44422b7cb
RicardoIngles <ringles@deakin.edu.au>
2023-03-22 22:57:56 +1100

    2.1p task
    accidently created folder as 1.1p realised mistake after built

---
+++ 1.1p/server.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const app=express();
+const port=8080
+
+app.get("/",function (req,res,)
+{
+    res.send('HELLO WORLD')
+})
+
+app.use('/',(req,res,next)=>{
+    console.log("NEW REQUEST AT "+ Date.now());
+    next();
+})
+app.listen(port, ()=> 
+{
+    console.log('Server online, Port ',port)
+})
